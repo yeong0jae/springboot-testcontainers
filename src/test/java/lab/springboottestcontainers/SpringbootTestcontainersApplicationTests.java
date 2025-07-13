7b9697856d67e60ed0cb61a60446f101a8fc2f36
Yeongjae Kim <sees77770@gmail.com>
2025-07-13 13:48:43 +0900

    init

---
+++ src/test/java/lab/springboottestcontainers/SpringbootTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.springboottestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
